309fb01e201f536b28e5fc1fb9c24da91edd367e
Shinmaul <maulana.husni@gmedia.co.id>
2018-05-04 18:05:56 +0700

    Update berkala

--- app/src/main/java/gmedia/net/id/phonebook/MainActivity.java
+++ app/src/main/java/gmedia/net/id/phonebook/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
@@ -11,6 +12,7 @@ import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -27,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
     private Spinner spnJenis;
     private Button btnSimpan;
     private final String formatDate = "yyyy-MM-dd";
+    private TextView tvIsiNama, tvIsiAlamat, tvIsiNomor, tvIsiTanggalLahir, tvIsiJenisKelamin, tvIsiJenis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void initUI() {
 
+        // lesson 1
         edtNama = (EditText) findViewById(R.id.edtNama);
         edtAlamat = (EditText) findViewById(R.id.edtAlamat);
         edtNomor = (EditText) findViewById(R.id.edtNomor);
@@ -48,6 +52,14 @@ public class MainActivity extends AppCompatActivity {
         rbPerempuan = (RadioButton) findViewById(R.id.rbPerempuan);
         spnJenis = (Spinner) findViewById(R.id.spnJenis);
         btnSimpan = (Button) findViewById(R.id.btnSimpan);
+
+        // lesson 2
+        tvIsiNama = (TextView) findViewById(R.id.tvIsiNama);
+        tvIsiAlamat = (TextView) findViewById(R.id.tvIsiAlamat);
+        tvIsiNomor = (TextView) findViewById(R.id.tvIsiNomor);
+        tvIsiTanggalLahir = (TextView) findViewById(R.id.tvIsiTanggal);
+        tvIsiJenisKelamin = (TextView) findViewById(R.id.tvIsiJenisKelamin);
+        tvIsiJenis = (TextView) findViewById(R.id.tvIsiJenis);
 
         initEvent();
     }
@@ -76,6 +88,65 @@ public class MainActivity extends AppCompatActivity {
                 new DatePickerDialog(MainActivity.this, dateSetListener, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DATE)).show();
             }
         });
+
+        String[] listJenis = new String[2];
+        listJenis[0] = "Telephone";
+        listJenis[1] = "Handphone";
+
+        ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, listJenis);
+        spnJenis.setAdapter(adapter);
+        spnJenis.setSelection(0);
+
+        btnSimpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                validasiSebelumSimpan();
+            }
+        });
+    }
+
+    private void validasiSebelumSimpan() {
+
+        if(edtNama.getText().toString().isEmpty()){
+
+            edtNama.setError("Nama harap diisi");
+            edtNama.requestFocus();
+            return;
+        }else{
+
+            edtNama.setError(null);
+        }
+
+        if(edtNomor.getText().toString().isEmpty()){
+
+            edtNomor.setError("Nomor harap diisi");
+            edtNomor.requestFocus();
+            return;
+        }else{
+
+            edtNomor.setError(null);
+        }
+
+        tvIsiNama.setText(edtNama.getText().toString());
+        tvIsiAlamat.setText(edtAlamat.getText().toString());
+        tvIsiNomor.setText(edtNomor.getText().toString());
+        tvIsiTanggalLahir.setText(edtDate.getText().toString());
+
+        String jenisKelamin = "";
+        if(rbLaki.isChecked()){
+            jenisKelamin = "Laki-laki";
+        }else{
+            jenisKelamin = "Perempuan";
+        }
+
+        tvIsiJenisKelamin.setText(jenisKelamin);
+
+        String jenisNomor = "";
+        if(spnJenis.getAdapter() != null){
+            jenisNomor = (String) spnJenis.getSelectedItem();
+        }
+        tvIsiJenis.setText(jenisNomor);
     }
 
 }
